16fc9c5d034cd2e78a00b035aa4fa55954a0e153
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2025-01-07 15:20:56 -0800

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+ let num1 = 2;
+ let num2 = 31;
+ let multiply = num1 * num2;
 
-// Write your code here
+ let random = Math.floor(Math.random() * 100) + 1;
+
+ let num3 = 19; 
+ let num4 = 5; 
+ let mod = num3 % num4; 
+ 
+
+let numbers = [2, 20, 5, 11];
+let max = Math.max(...numbers);
